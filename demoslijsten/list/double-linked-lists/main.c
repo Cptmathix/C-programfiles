af34003fd65b15c34187bf4c9abfc9fe9b71d9d6
mathias spanhove <mathias.spanhove@hotmail.be>
2014-12-10 17:14:30 +0100

    minor changes

--- demoslijsten/list/double-linked-lists/main.c
+++ demoslijsten/list/double-linked-lists/main.c
@@ -8,14 +8,22 @@ int main()
     struct DList* list = dlist_create();
     
     dlist_print(list);
-    
+    printf("list length = %d\n", dlist_length(list));
     dlist_append(list, 5);
+    printf("list length = %d\n", dlist_length(list));
     dlist_append(list, 7);
+    printf("list length = %d\n", dlist_length(list));
     dlist_append(list, 8);
+    printf("list length = %d\n", dlist_length(list));
     dlist_print(list);
 	dlist_print_reverse(list);
 	
 	dlist_remove(list, 1);
+	printf("list length = %d\n", dlist_length(list));
+	dlist_print(list);
+	dlist_print_reverse(list);
+	dlist_insert(list, 0, 5);
+	printf("list length = %d\n", dlist_length(list));
 	dlist_print(list);
 	dlist_print_reverse(list);
 	
